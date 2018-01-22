45f7536567fb81c46dc8c0379377ddf5ddbba253
Nikhitha VK <NikhithaVadla.Konda@thomsonreuters.com>
2018-01-22 10:42:46 -0600

    getWork

--- http-app/src/app/app.component.ts
+++ http-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Http in Angular';
+  name: string = '';
+  //profiles: any;
+  found: boolean;
+  reknown: string;
+
+
+
+constructor( private httpClient:HttpClient ){}
+ 
+  onKeyUp(event:any){
+    console.log('test', event);
+    this.name = event.target.value;
+    this.found = false;
+  }
+
+
+  getData(){
+    this.httpClient.get('https://my-json-server.typicode.com/vknikhitha/ng4-http-get-post/profiles/?name='+this.name)
+      .subscribe(
+        (data:any[])=> {
+          if(data.length){
+            this.reknown = data[0].reknown;
+            this.found = true;
+          }
+          //this.profiles = data;
+        }
+    
+      )
+  }
+
 }
